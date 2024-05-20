ad4b412986b1069d69f5feb1869df9b7ed06bbdc
Kelvin Wambua <62960869+kelvinwambua@users.noreply.github.com>
2024-05-20 22:29:00 +0300

    Delete src directory

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import {Request, Response, Express} from 'express';
-import { Connection, EntityManager, IDatabaseDriver } from '@mikro-orm/core';
-
-export type MyContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request & { session: Express.Session };
-  res: Response;
-};
